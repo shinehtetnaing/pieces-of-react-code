9a2c712ba9655c7a941c454d872a630b76dc1579
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-02-01 22:47:48 +0630

    HOC example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+// with part of the name is general name convention
+const withMousePosition = (WrappedComponent) => {
+  return (props) => {
+
+    const [mousePositioin, setMousePosition] = useState({
+      x: 0,
+      y: 0,
+    })
+
+    useEffect(() => {
+      const handleMousePositionChange = (e) => {
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      };
+
+      window.addEventListener("mousemove", handleMousePositionChange);
+
+      return () => {
+        window.removeEventListener("mousemove", handleMousePositionChange);
+      };
+    }, []);
+
+    return (
+      <WrappedComponent {...props} mousePositioin={mousePositioin} />
+    )
+  }
+}
+
+const PanelMouseLogger = ({ mousePositioin }) => {
+  if (!mousePositioin) {
+    return null;
+  }
+  return (
+    <div className="BasicTracker">
+      <p>Mouse Position :</p>
+      <div className="Row">
+        <span>x: {mousePositioin.x}</span>
+        <span>y: {mousePositioin.y}</span>
+      </div>
+    </div>
+  );
+};
+
+const PointMouseLogger = ({ mousePositioin }) => {
+  if (!mousePositioin) {
+    return null;
+  }
+  return (
+    <p>
+      ({mousePositioin.x}, {mousePositioin.y})
+    </p>
+  );
+};
+
+//to enhence components
+const PanelMouseTracker = withMousePosition(PanelMouseLogger);
+const PointMouseTracker = withMousePosition(PointMouseLogger);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="Header">Little Lemon Restaurant</header>
+      <PanelMouseTracker />
+      <PointMouseTracker />
     </div>
   );
 }
