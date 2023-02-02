59ab4a37ea6dc691029e607311b1462e5c70449d
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-02-02 21:38:05 +0630

    render props example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const DataFetcher = ({ render, url }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (url.includes("desserts")) {
+      setData(["cake", "ice cream", "pie", "brownie"]);
+    } else {
+      setData(["water", "soda", "juice"]);
+    }
+  }, []);
+
+  return render(data);
+};
+
+const DessertsCount = () => {
+  return (
+    <DataFetcher
+      url="https://littlelemon/desserts"
+      render={(data) => <p>{data.length} desserts</p>}
+    />
+  );
+};
+
+const DrinksCount = () => {
+  return (
+    <DataFetcher
+      url="https://littlelemon/drinks"
+      render={(data) => <h3>{data.length} drinks</h3>}
+    />
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="Header">Little Lemon Restaurant</header>
+      <DessertsCount />
+      <DrinksCount />
     </div>
   );
 }
