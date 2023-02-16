bd5401b5335985d6ae5b518fe62dc153fc4b1059
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-02-16 10:48:51 +0630

    useReducer hook example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  if (action.type === 'buy_ingredients') return {money: state.money - 10};
+  if (action.type === 'sell_a_meal') return {money: state.money + 10};
+  if (action.type === 'celebrity_visit') return {money: state.money + 5000};
+  return state;
+};
 
 function App() {
+  const initialState = {money: 100};
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wallet: {state.money}</h1>
+      <button onClick={()=> dispatch({type: 'buy_ingredients'})}>Shopping for veggies!</button>
+      <button onClick={()=> dispatch({type: 'sell_a_meal'})}>Serve a meal to the customer</button>
+      <button onClick={()=> dispatch({type: 'celebrity_visit'})}>Celebrity visit</button>
     </div>
   );
 }
