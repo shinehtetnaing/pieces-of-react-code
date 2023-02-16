b8a84f12e7028f0f2e4c5fcc8d185616aad13b63
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-02-16 11:53:28 +0630

    React.cloneElement and React.Children.map example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+
+const Row = ({ children, spacing }) => {
+
+  const childStyle = {
+    marginLeft : `${spacing}px`,
+  }
+  return (
+    <div className='Row'>
+      {React.Children.map(children, (child, index) => {
+        return React.cloneElement(child, {
+          //new style props that will merge the previous style.
+          style : {
+            ...child.props.style,
+            //if the element is not a first child, then I'll also merge the child style object that contains the margin left.
+            ...(index > 0 ? childStyle : {})
+          }
+        });
+      })}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row spacing={32}>
+        <p>Pizza</p>
+        <p>2</p>
+        <p>30$</p>
+        <p>18:30</p>
+        <p>John</p>
+      </Row>
     </div>
   );
 }
