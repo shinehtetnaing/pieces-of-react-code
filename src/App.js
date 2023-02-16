4186364155b19cd018f406daffba796348d310f8
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-02-16 10:57:38 +0630

    useRef hook example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useRef} from 'react';
 
 function App() {
+  const formInputRef = useRef(null);
+
+  const focusInput = () => {
+    formInputRef.current.focus();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1>Using useRef to access underlying DOM</h1>
+        <input type='text' ref={formInputRef} />
+        <button onClick={focusInput}>
+          Focus Input
+        </button>
+      </>
     </div>
   );
 }
