d7e1e955d155073a8c2b7a22c770cb0795f174c4
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-02-16 21:14:01 +0630

    context api example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { UserProvider, useUser } from "./UserContext";
+
+const LoggedInUser = () => {
+  const { user } = useUser();
+  return (
+    <p>
+      Hello <span className="UserName">{user.name}</span>
+    </p>
+  );
+};
+
+const Header = () => {
+  return (
+    <header>
+      <h2 style={{ flex: 1 }}>Blog App</h2>
+      <LoggedInUser />
+    </header>
+  );
+};
+
+const Page = () => {
+  const { user } = useUser();
+  return (
+    <div>
+      <h2>Page Header?</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+      <p>Written By: {user.name}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Page />
     </div>
   );
 }
 
-export default App;
+function Root() {
+  return (
+    <UserProvider>
+      <App />
+    </UserProvider>
+  );
+}
+
+export default Root;
